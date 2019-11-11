65f016c1d7b7cb797a179a500ca94bd8e8995baf
Anthony Ferraioli <amf7619@g.rit.edu>
2019-11-11 10:50:20 -0500

    Added UI singleton

--- include/glm/out/build/x64-Debug/cmake_install.cmake
+++ include/glm/out/build/x64-Debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/Profiles/dxs4043/source/repos/Simplex_2191/include/glm
+# Install script for directory: C:/Users/Anthony/Desktop/School/Data_Structures_&_Alg_Games_II/SteveAttack/include/glm
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "d:/profiles/dxs4043/source/repos/simplex_2191/include/glm/out/install/x64-Debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/Anthony/Desktop/School/Data_Structures_&_Alg_Games_II/SteveAttack/include/glm/out/install/x64-Debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Profiles/dxs4043/source/repos/Simplex_2191/include/glm/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Anthony/Desktop/School/Data_Structures_&_Alg_Games_II/SteveAttack/include/glm/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
